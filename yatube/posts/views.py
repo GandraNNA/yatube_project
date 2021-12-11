e25e7b554a5a0c5bdf77c72fce695cb2e076af07
GandraNNA <praskovia.gandrabura@yandex.ru>
2021-12-11 22:46:03 +0300

    added urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Main page
+def index(request):
+    return HttpResponse('Main page')
+
+
+# Page with posts, filtered by groups
+def group_posts(request, slug):
+    return HttpResponse('Posts, filtered by groups')
