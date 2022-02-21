724110e71eef0583080c245020f5c31843c159f2
GandraNNA <praskovia.gandrabura@yandex.ru>
2022-02-22 02:44:40 +0300

    + add many things

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,34 +1,48 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(
+        'Заголовок',
+        max_length=200
+    )
+    slug = models.SlugField(
+        'Идентификатор группы',
+        max_length=200,
+        unique=True
+    )
+    description = models.TextField('Описание')
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    # Тип: TextField
-    text = models.TextField()
-    # Тип поля: DateTimeField, для хранения даты и времени;
-    # параметр auto_now_add определяет, что в поле будет автоматически
-    # подставлено время и дата создания новой записи
-    pub_date = models.DateTimeField(auto_now_add=True)
-    # Тип: ForeignKey, ссылка на модель User
+    text = models.TextField('Текст поста')
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True,
         related_name='posts'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        related_name='order_group_posts'
     )
+
+    class Meta:
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        # выводим текст поста
+        return self.text
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,43 +1,169 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+# from django.core.mail import send_mail
+from django.urls import reverse_lazy
 
-from .models import Post, Group
+from .forms import PostForm
+from .models import Group, Post, User
 
 
-# Main page
 def index(request):
+    template = 'posts/index.html'
     title = 'Последние обновления на сайте'
     text = 'Последние обновления на сайте'
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к
-    # меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
         'title': title,
         'text': text,
     }
-    return render(request, 'posts/index.html', context)
+    return render(
+        request,
+        template,
+        context
+    )
 
 
-# View-функция для страницы сообщества:
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Записи сообщества ' + slug
-    text = 'Лев Толстой – зеркало русской революции.'
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    post_list = group.order_group_posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
-        'title': title,
-        'text': text,
+        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    return render(
+        request,
+        template,
+        context
+    )
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    author = get_object_or_404(User, username=username)
+    title = 'Профайл пользователя ' + author.get_full_name()
+    post_user_list = Post.objects.select_related('author', 'group').all()
+    number_of_posts = post_user_list.count()
+    post_list = author.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'title': title,
+        'author': author,
+        'number_of_posts': number_of_posts,
+        'post_list': post_list,
+    }
+    return render(request,
+                  template,
+                  context
+                  )
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = Post.objects.filter(pk=post_id).first()
+    title = 'Пост ' + post.text[:15]
+    context = {
+        'post': post,
+        'title': title,
+    }
+    return render(request,
+                  template,
+                  context
+                  )
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create.html'
+    title = 'Новая запись'
+    header = 'Добавить запись'
+    form = PostForm(data=request.POST)
+    id_group = Group.objects.all()
+    if request.method == 'POST':
+
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return redirect(
+                reverse_lazy(
+                    'posts:profile',
+                    args=(request.user.username,)
+                )
+            )
+        else:
+            form = render(request,
+                          template,
+                          {'form': form}
+                          )
+
+    context = {
+        'title': title,
+        'header': header,
+        'id_group': id_group,
+        'form': form,
+    }
+    return render(
+        request,
+        template,
+        context
+    )
+
+
+@login_required
+def post_edit(request, post_id):
+    template = 'posts/create.html',
+    title = 'Редактировать запись'
+    header = 'Редактировать запись'
+    post = get_object_or_404(
+        Post,
+        pk=post_id
+    )
+
+    if post.author != request.user:
+        return redirect(reverse_lazy(
+            'posts:post_detail',
+            args=(post.pk,)))
+
+    if request.method == 'POST':
+        form = PostForm(
+            data=request.POST,
+            instance=post
+        )
+        if form.is_valid():
+            form.save()
+            return redirect(
+                reverse_lazy(
+                    'posts:post_detail',
+                    args=(post.pk,))
+            )
+        else:
+            return render(
+                request,
+                template,
+                {'form': form}
+            )
+
+    form = PostForm(initial={'text': post.text})
+    context = {
+        'title': title,
+        'header': header,
+        'is_edit': True,
+        'form': form,
+    }
+    return render(
+        request,
+        template,
+        context
+    )
