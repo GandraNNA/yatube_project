59d772a9473d2e2c5555f7d7ed84ef75f096d03d
GandraNNA <praskovia.gandrabura@yandex.ru>
2022-05-13 02:31:30 +0300

    add Comment to forms

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Group, Post
+from .models import Group, Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -12,8 +12,15 @@ class PostForm(forms.ModelForm):
             'image'
         )
 
-    text = forms.Textarea()
-    group = forms.ModelChoiceField(
-        queryset=Group.objects.all(),
-        required=False
-    )
+        text = forms.Textarea()
+        group = forms.ModelChoiceField(
+            queryset=Group.objects.all(),
+            required=False
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        text = forms.Textarea()
